3a37d54fb23781615919c0e9b8a5ed0526f17d09
as-korchagin <as.korchagin@yandex.ru>
2018-03-02 09:52:13 +0300

    some changes i don't really know what they about

--- 2.2/1.small_fib_num/main.pl
+++ 2.2/1.small_fib_num/main.pl
@@ -3,16 +3,19 @@ use strict;
 use warnings;
 $|++;
 
+
 sub fib {
-    my ($fib1, $fib2) = (0, 1);
-    if ($_[0] == 0 or $_[0] == 1) {
-        return $_[0];
+    my ($fib1, $fib2, $fib_num) = (0, 1, shift);
+    if ($fib_num == 0 or $fib_num == 1) {
+        return $fib_num;
     }
-    for (my $i = 0; $i < $_[0]; $i++) {
+    for (my $i = 0; $i < $fib_num; $i++) {
         ($fib1, $fib2) = ($fib2, $fib1 + $fib2);
     }
     return $fib1;
 }
 
-chomp(my $var = <STDIN>);
-say fib($var);
+
+my $var = '';
+chomp($var = <STDIN>);
+print fib($var) . "\n";

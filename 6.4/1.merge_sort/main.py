defa5ec597777cfe998b08dd3c57359ddaeb897f
as-korchagin <as.korchagin@yandex.ru>
2018-03-12 22:35:44 +0300

    Some changes, but still not working

--- 6.4/1.merge_sort/main.py
+++ 6.4/1.merge_sort/main.py
@@ -3,12 +3,19 @@ def merge(merge_item_1, merge_item_2):
     item_1_current = 0
     item_2_current = 0
     while len(merged) != (len(merge_item_1) + len(merge_item_2)):
-        if item_1_current < len(merge_item_1) and merge_item_1[item_1_current] <= merge_item_2[item_2_current]:
-            merged.append(merge_item_1[item_1_current])
-            item_1_current += 1
-        else:
+        if item_1_current < len(merge_item_1) and item_2_current < len(merge_item_2):
+            if merge_item_1[item_1_current] <= merge_item_2[item_2_current]:
+                merged.append(merge_item_1[item_1_current])
+                item_1_current += 1
+            else:
+                merged.append(merge_item_1[item_2_current])
+                item_2_current += 1
+        elif item_1_current >= len(merge_item_1):
             merged.append(merge_item_2[item_2_current])
             item_2_current += 1
+        else:
+            merged.append(merge_item_1[item_1_current])
+            item_1_current += 1
     return merged
 
 
@@ -16,6 +23,7 @@ def merge_sort(array, left, right):
     if left < right:
         median = int((left + right) / 2)
         array[left: right + 1] = merge(merge_sort(array, left, median), merge_sort(array, median + 1, right))
+    #     TODO:some error here
     return array[left: right + 1]
 
 
@@ -25,5 +33,5 @@ if __name__ == "__main__":
     assert len(array) == array_length, "Array length invalid"
     print(merge_sort(array, 0, array_length - 1))
 # import random
-# for _ in range(100):
-#     print(random.randint(1, 100), end=' ')
+# for _ in range(15):
+#     print(random.randint(1, 50), end=' ')

5773b142b77ba2505e1f1967aa59fd1f1c9604fc
as-korchagin <as.korchagin@yandex.ru>
2018-03-19 19:35:42 +0300

    quick sort fixed

--- 6.5/1.quick_sort/main.py
+++ 6.5/1.quick_sort/main.py
@@ -1,25 +1,20 @@
 def partition(array, left, right):
-    swapped = False
     core = array[left]
     border = left
     for id in range(left + 1, right + 1):
         if array[id] <= core:
             border += 1
-            swapped = True
             array[border], array[id] = array[id], array[border]
     array[left], array[border] = array[border], array[left]
-    return {"border": border,
-            "swapped": swapped}
+    return border
 
 
 def quick_sort(array, left, right):
     if left >= right:
         return
     core = partition(array, left, right)
-    if not core.get('swapped'):
-        return
-    quick_sort(array, left, core.get('border') - 1)
-    quick_sort(array, core.get('border') + 1, right)
+    quick_sort(array, left, core - 1)
+    quick_sort(array, core, right)
 
 
 def dots_and_lines(lines, dots):
@@ -31,6 +26,6 @@ if __name__ == "__main__":
     # lines = list(list(map(int, input().split())) for i in range(number_of_lines))
     # dots = list(map(int, input().split()))
     # dots_and_lines(lines, dots)
-    arr = [5, 7, 2, 5, 8, 9, 223, 6, 5, 6, -1]
-    quick_sort(arr, 0, 10)
+    arr = [9, -6, 0, -4, -9, 8, 9, 4, 0, 9, -4, 4, -6, -3, 0, -10, 6, 5, 8, -9, 6, 10, 5, -7, 7, -2, 7, -2, -4, 4, 4, -10, -9, -7, -8, -10, 3, 8, -5, -2, 3, 8, -7, -8, 4, -4, -8, -7, -8, 0, -4, 0, -5, 6, -7, -5, 6, 10, 7, -8, 1, -1, 2, -7, 7, 10, -4, 2, 3, 9, 9, -1, -8, 2, -2, -2, 0, -3, 2, 0, 0, 10, -3, -5, -4, -9, -7, 7, 9, -8, 10, -7, -6, -6, 5, 9, 0, -7, 6, -9]
+    quick_sort(arr, 0, len(arr) - 1)
     print(arr)
